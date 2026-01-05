ecbbf693202a5d08b3917fc4832cb51aa8675346
Fernando <vluisfernando62@gmail.com>
2026-01-05 11:54:32 -0400

    feat(Region): se agrego las rutas manejar la informacion de regiones

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\RegionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('regions', RegionController::class);
+});
