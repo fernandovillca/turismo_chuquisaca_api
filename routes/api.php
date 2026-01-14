2d305e8bdb9a6575d65e096cb850ea612e9f5718
Fernando <vluisfernando62@gmail.com>
2026-01-14 10:19:03 -0400

    fix(api): se quito el codigo comentado y no usado

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,7 @@ use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\CommunityController;
 use App\Http\Controllers\Api\V1\MunicipalityController;
 use App\Http\Controllers\Api\V1\RegionController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('regions', RegionController::class);
