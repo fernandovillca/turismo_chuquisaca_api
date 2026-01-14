3100f525f6057dd506906c4e2c8f4a6753c11f37
fernando <vluisfernando62@gmail.com>
2026-01-14 16:45:34 -0700

    feat: se agrego la migracion y el modelo Language para la tabla de lenguajes

---
+++ database/migrations/2026_01_14_233732_create_languages_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('languages', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('code', 2)
+                ->unique(); // es, en, fr, qu
+
+            $table->string('name', 50)
+                ->unique();
+
+            $table->boolean('is_active')
+                ->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('languages');
+    }
+};
